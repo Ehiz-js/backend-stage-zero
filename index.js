aeab48bf1afe98fb27f55abd2fcb31b4ddb0d21b
Ehiz-js <azamegbesamuel@gmail.com>
2025-10-16 22:55:57 +0100

    add: dynamic port for prod

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import morgan from "morgan";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const API_URL = process.env.API_URL;
 
 //middleware
